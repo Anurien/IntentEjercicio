2da0fb9f4bf0225c065e51494ca0a16ab9d87522
Nuria <ngarciacobas@danielcastelao.org>
2023-01-16 10:59:21 +0100

    Intent para abrir la camara de fotos y recuperar la foto que saque al activity

--- app/src/main/java/com/dam2/chatgpt/MainActivity.kt
+++ app/src/main/java/com/dam2/chatgpt/MainActivity.kt
@@ -3,10 +3,13 @@ package com.dam2.chatgpt
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -17,15 +20,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val enviar: Button = findViewById(R.id.start)
-        val textView = findViewById<TextView>(R.id.texto)
-        val url = "https://github.com/GorillaGrip/IntentEjercicio"
-        textView.text = url
+        //En el listener ponemos el intent para capturar una foto
         enviar.setOnClickListener {
-            val webpage: Uri = Uri.parse(url)
-            val intent = Intent(Intent.ACTION_VIEW, webpage)
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
             if (intent.resolveActivity(packageManager) != null) {
-                startActivity(intent)
+                startActivityForResult(intent, REQUEST_IMAGE_CAPTURE)
             }
         }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        val imageView = findViewById<ImageView>(R.id.texto)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+            //REcoge la foto y la muestra por pantalla
+            val thumbnail = data?.extras!!.get("data") as Bitmap
+            imageView.setImageBitmap(thumbnail)
+            // Do other work with full size photo saved in locationForPhotos
+        }
+    }
+
+    companion object {
+        const val REQUEST_IMAGE_CAPTURE = 1
+    }
+
 }
