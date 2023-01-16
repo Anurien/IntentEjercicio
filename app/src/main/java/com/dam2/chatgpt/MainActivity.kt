9c5def930be5346b8fa15ce3e66abd0260ff1ed6
Nuria <ngarciacobas@danielcastelao.org>
2023-01-16 10:26:17 +0100

    Intent para abrir y marcar un numero de telefono

--- app/src/main/java/com/dam2/chatgpt/MainActivity.kt
+++ app/src/main/java/com/dam2/chatgpt/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.dam2.chatgpt
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
@@ -10,57 +12,21 @@ import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("QueryPermissionsNeeded")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var enviar: Button = findViewById(R.id.start)
+        val enviar: Button = findViewById(R.id.start)
+        val textView = findViewById<TextView>(R.id.texto)
+        val phoneNumber = "123456789"
+        textView.text = phoneNumber
         enviar.setOnClickListener {
-            val intento1 = Intent(this, SecondActivity::class.java)
-            // Añade datos adicionales al Intent
-            intento1.putExtra("EXTRA_MESSAGE", 8)
-            intento1.putExtra("EXTRA_MESSAGE1", 7)
-            // Inicia la segunda actividad
-            startActivityForResult(intento1, REQUEST_EXTRA_MESSAGE)
-            startActivityForResult(intento1, REQUEST_PERMISSION)
-            startActivityForResult(intento1, REQUEST_TO_POST)
+            val intent = Intent(Intent.ACTION_DIAL).apply {
+                data = Uri.parse("tel:$phoneNumber")
+            }
+            if (intent.resolveActivity(packageManager) != null) {
+                startActivity(intent)
+            }
         }
-    }
-    // recoje el intent cuando hacemos setResult() en la SecondActivity
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        // Definimos el text view para mostrar el dato que nos manda la Second
-        val textView1 = findViewById<TextView>(R.id.texto)
-        val textView2 = findViewById<TextView>(R.id.texto3)
-        val textView3 = findViewById<TextView>(R.id.texto4)
-        // Comprueba que el resultado es OK
-        if (resultCode != Activity.RESULT_OK) return
-        Log.d("asd", requestCode.toString())
-        // Puedo distinguir diferentes acciones según el requestCode
-        when (requestCode) {
-            REQUEST_EXTRA_MESSAGE -> {
-                // si el intent no es null muestro el resultado
-                if (data != null) {
-                    Log.d("asd", "recibido")
-                    textView1.text = data.getIntExtra("suma",0).toString()
-                }; }
-            REQUEST_PERMISSION -> {
-                if (data != null) {
-                    Log.d("asd", "recibido")
-                    textView2.text = data.getIntExtra("resta",0).toString()
-                }; }
-
-            REQUEST_TO_POST -> {
-                if (data != null) {
-                    Log.d("asd", "recibido")
-                    textView3.text = data.getIntExtra("multi",0).toString()
-                }; }
-
-        }
-    }
-
-    companion object {
-        const val REQUEST_EXTRA_MESSAGE = 1
-        const val REQUEST_PERMISSION = 2
-        const val REQUEST_TO_POST = 3
     }
 }
